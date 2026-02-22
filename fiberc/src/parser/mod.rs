9becb419fad2668242feb63db583eb5430f4c2c6
santi34mg <ssilvera@correo.um.edu.uy>
2026-02-22 00:41:56 -0300

    Removes legacy parser modules and refactors for new AST integration

--- fiberc/src/parser/mod.rs
+++ fiberc/src/parser/mod.rs
@@ -1,16 +1,4 @@
-pub mod ast;
-pub mod expression;
-pub mod function;
 pub mod parser;
-pub mod statement;
 pub mod test;
-pub mod types;
-pub mod variable_declaration;
 
-pub use ast::Ast;
-pub use expression::Expression;
-pub use function::{Function, FunctionBody, FunctionParameter, FunctionSignature};
 pub use parser::Parser;
-pub use statement::Statement;
-pub use types::TypeIdentifier;
-pub use variable_declaration::VariableDeclaration;
