c35eb6a2a3cd7cea33c98644828683726d585fc2
santi34mg <ssilvera@correo.um.edu.uy>
2026-02-22 00:41:56 -0300

    Simplifies main binary entrypoint for new lib-based architecture

--- fiberc/src/main.rs
+++ fiberc/src/main.rs
@@ -1,14 +1,4 @@
-mod cli;
-mod driver;
-pub mod hir;
-mod lexer;
-pub mod lowering;
-mod parser;
-pub mod semantic_analysis;
-mod token;
-
 fn main() {
-    let args = cli::parse_args();
-
-    cli::exec_command(args);
+    let args = fiberc::cli::parse_args();
+    fiberc::cli::exec_command(args);
 }
