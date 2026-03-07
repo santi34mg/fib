202e647501905d269bf3403e7c3e688b95845163
santi34mg <ssilvera@correo.um.edu.uy>
2026-03-07 16:07:49 -0300

    updated ast

--- fiberc/src/ast/ast.rs
+++ fiberc/src/ast/ast.rs
@@ -1,40 +1,29 @@
+use core::fmt;
+
+use crate::token::builtin::BuiltinType;
+use crate::token::Operator;
+use crate::token::identifier::Identifier;
+use crate::token::literal::Literal;
+
 #[derive(Debug, Clone)]
 pub struct Ast {
-    pub program: ProgramNode,
+    pub declarations: Vec<DeclarationNode>,
 }
 
 impl Ast {
     pub fn new() -> Self {
         return Self {
-            program: ProgramNode {
-                modules: Vec::new(),
-            },
+            declarations: Vec::new(),
         };
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct ProgramNode {
-    pub modules: Vec<ModuleNode>,
-}
-
-#[derive(Debug, Clone)]
-pub struct ModuleNode {
-    pub declarations: Vec<DeclarationNode>,
-}
 
 #[derive(Debug, Clone)]
 pub enum DeclarationNode {
     FunctionDeclaration(FunctionDeclaration),
     TypeDeclaration(TypeDeclaration),
-    ModuleDeclaration(ModuleDeclaration),
     Statement(StatementNode),
-}
-
-#[derive(Debug, Clone)]
-pub struct ModuleDeclaration {
-    pub name: String,
-    pub uses: Vec<String>,
 }
 
 #[derive(Debug, Clone)]
@@ -42,7 +31,7 @@ pub enum StatementNode {
     VariableDeclaration(VariableDeclaration),
     ExpressionStatement(Expression),
     Assignment {
-        identifier: String,
+        identifier: Identifier,
         expr: Expression,
     },
     Return(Option<Expression>),
@@ -62,62 +51,91 @@ pub enum StatementNode {
 #[derive(Debug, Clone)]
 pub struct FunctionDeclaration {
     pub signature: FunctionSignature,
-    pub body: Option<FunctionBody>,
+    pub body: FunctionBody,
 }
 
 #[derive(Debug, Clone)]
 pub struct FunctionSignature {
-    pub name: String,
+    pub name: Identifier,
     pub parameters: Vec<FunctionParameter>,
-    pub return_type: Option<TypeIdentifier>,
+    pub return_type: Option<TypeExpression>,
 }
 
 #[derive(Debug, Clone)]
 pub struct FunctionParameter {
-    pub parameter_name: String,
-    pub parameter_type: TypeIdentifier,
+    pub parameter_name: Identifier,
+    pub parameter_type: TypeExpression,
 }
 
 #[derive(Debug, Clone)]
-pub enum FunctionBody {
-    Statements(Vec<StatementNode>),
+pub struct FunctionBody {
+    pub statements: Vec<StatementNode>,
 }
 
 #[derive(Debug, Clone)]
-pub struct TypeDeclaration {}
+pub struct TypeDeclaration {
+    pub name: Identifier,
+    pub type_expression: TypeExpression,
+}
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum TypeIdentifier {
-    UserDefinedType(String),
-    Unit,
-    Integer,
-    Float,
-    String,
-    Boolean,
-    Character,
+pub enum TypeExpression {
+    Builtin(BuiltinType),
+    Identifier(Identifier),
     Function {
-        argument_types: Vec<TypeIdentifier>,
-        return_type: Box<TypeIdentifier>,
+        argument_types: Vec<TypeExpression>,
+        return_type: Box<TypeExpression>,
     },
     Pointer {
         pointer_variant: PointerVariant,
-        pointed_type: Box<TypeIdentifier>,
+        pointed_type: Box<TypeExpression>,
     },
     Struct {
         fields: Vec<Field>,
     },
-    Variant {
-        fields: Vec<Field>,
-    },
-    Dynamic,
-    Blob,
-    Never,
 }
+
+impl fmt::Display for TypeExpression {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TypeExpression::Builtin(builtin_type) => {
+                write!(f, "{}", builtin_type)?;
+            }
+            TypeExpression::Identifier(identifier) => {
+                write!(f, "{}", identifier)?;
+            }
+            TypeExpression::Function { argument_types, return_type } => {
+                write!(f, "function({:?}) -> {}", argument_types, return_type)?;
+            }
+            TypeExpression::Struct { fields } => {
+                write!(f, "struct {{ {:?} }}", fields)?;
+            }
+            TypeExpression::Pointer { pointer_variant, pointed_type } => {
+                match pointer_variant {
+                    PointerVariant::Unique => {
+                        write!(f, "unique &{}", *pointed_type)?;
+                    }
+                    PointerVariant::Shared => {
+                        write!(f, "shared &{}", *pointed_type)?;
+                    }
+                    PointerVariant::Weak => {
+                        write!(f, "weak &{}", *pointed_type)?;
+                    }
+                    PointerVariant::Raw => {
+                        write!(f, "&{}", *pointed_type)?;
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Field {
-    pub(crate) label: String,
-    pub(crate) type_id: TypeIdentifier,
+    pub(crate) label: Identifier,
+    pub(crate) type_id: TypeExpression,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -130,15 +148,15 @@ pub enum PointerVariant {
 
 #[derive(Debug, Clone)]
 pub struct VariableDeclaration {
-    pub identifier: String,
-    pub variable_type: Option<TypeIdentifier>,
+    pub identifier: Identifier,
+    pub variable_type: Option<TypeExpression>,
     pub expression: Option<Expression>,
 }
 
 impl VariableDeclaration {
     pub fn new(
-        identifier: String,
-        variable_type: Option<TypeIdentifier>,
+        identifier: Identifier,
+        variable_type: Option<TypeExpression>,
         expression: Option<Expression>,
     ) -> Self {
         Self {
@@ -149,8 +167,6 @@ impl VariableDeclaration {
     }
 }
 
-use crate::token::Operator;
-use crate::token::literal::Literal;
 
 #[derive(Debug, Clone)]
 pub enum Expression {
@@ -165,7 +181,7 @@ pub enum Expression {
     },
     Literal(Literal),
     // The identifier expression contains the name of the identifier as a string
-    Identifier(String),
+    Identifier(Identifier),
     Grouping(Box<Expression>),
     Call {
         callee: Box<Expression>,
