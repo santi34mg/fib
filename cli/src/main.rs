8bbbcc43a58700f6ed1684cec4ca24f8e6fb3a9f
santi34mg <ssilvera@correo.um.edu.uy>
2026-03-07 16:04:32 -0300

    updated cli commands and added easy to modify templates

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,107 +1,137 @@
 use clap::{Parser, Subcommand};
-use std::{fs, path::PathBuf, process};
+use serde::Deserialize;
+use std::{error::Error, fs, path::PathBuf, process};
+use tera::{Context, Tera};
 
 use fiberc;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+const CONFIG_PATH: &str = "fiber.toml";
+const SRC_ROOT: &str = "src";
+const MAIN_ENTRY: &str = "main.fib";
+const CONFIG_TEMPLATE_STR: &str = include_str!("../resources/fiber.toml.template");
+const SOURCE_TEMPLATE_STR: &str = include_str!("../resources/main.fib.template");
 
 #[derive(Parser)]
 #[command(name = "fiber", about = "Fiber project CLI")]
 struct Cli {
-	#[command(subcommand)]
-	command: Option<Commands>,
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
 
-	#[arg(short, long, default_value_t = false)]
-	debug: bool,
+#[derive(Debug, Deserialize)]
+struct Cfg {
+    package: PackageConfig,
+}
+
+#[derive(Debug, Deserialize)]
+struct PackageConfig {
+    name: String,
+    version: String,
+    src_root: String,
+    main_module: String,
+    target: String,
 }
 
 #[derive(Subcommand)]
 enum Commands {
-	Compile { file: Option<PathBuf> },
-	Init { dir: PathBuf },
-	Deps { urls: Vec<String>, dest: Option<PathBuf> },
+    Compile {
+        file: Option<PathBuf>,
+    },
+    Init {
+        dir: PathBuf,
+    },
+    Deps {
+        urls: Vec<String>,
+        dest: Option<PathBuf>,
+    },
 }
 
 fn main() {
-	let cli = Cli::parse();
-	match cli.command {
-		Some(Commands::Init { dir }) => init_command(&dir),
-		Some(Commands::Compile { file }) => compile_command(file, cli.debug),
-		Some(Commands::Deps { urls, dest }) => deps_command(urls, dest),
-		None => {
-			eprintln!("Specify a command: compile, init, deps");
-			process::exit(1);
-		}
-	}
+    let cli = Cli::parse();
+    let command_result = match cli.command {
+        Some(Commands::Init { dir }) => init_command(dir),
+        Some(Commands::Compile { file }) => compile_command(file),
+        Some(Commands::Deps { urls, dest }) => deps_command(urls, dest),
+        None => {
+            eprintln!("Specify a command: compile, init, deps");
+            process::exit(1);
+        }
+    };
+    match command_result {
+        Ok(_) => {},
+        Err(e) => eprintln!("Command failed: \n{}", e)
+    }
 }
 
-fn compile_command(file: Option<PathBuf>, debug: bool) {
-	let file = match file {
-		Some(f) => f,
-		None => {
-			// Try to read fiber.toml for main_module_path, otherwise default to main.fib
-			match fs::read_to_string("fiber.toml") {
-				Ok(s) => {
-					#[derive(serde::Deserialize)]
-					struct Cfg {
-						main_module_path: String,
-					}
-					if let Ok(cfg) = toml::from_str::<Cfg>(&s) {
-						PathBuf::from(cfg.main_module_path)
-					} else {
-						PathBuf::from("main.fib")
-					}
-				}
-				Err(_) => PathBuf::from("main.fib"),
-			}
-		}
-	};
+fn compile_command(file: Option<PathBuf>) -> Result<(), Box<dyn std::error::Error>> {
+    let config: Cfg = toml::from_str::<Cfg>(&fs::read_to_string(CONFIG_PATH)?)?;
+    let file = match file {
+        Some(f) => f,
+        None => PathBuf::from(format!("{}/{}", config.package.src_root, config.package.main_module)),
+    };
 
-	match fiberc::compile_project(&file, debug) {
-		Ok(()) => println!("Compilation succeeded."),
-		Err(e) => {
-			eprintln!("Compilation failed: {}", e);
-			process::exit(1);
-		}
-	}
+    println!("Compiling project {} with fiber version {}", config.package.name, config.package.version);
+    fiberc::compile_project(&file, config.package.target)
 }
 
-fn init_command(dir: &PathBuf) {
-	fs::create_dir_all(&dir).unwrap_or_else(|e| {
-		eprintln!("Error creating project directory: {e}");
-		process::exit(1);
-	});
-	let config = "main_module_path = \"main.fib\"\nis_debug = true";
-	fs::write(dir.join("fiber.toml"), config).unwrap();
-	fs::write(dir.join("main.fib"), "// Fiber entry point\n").unwrap();
-	println!("Initialized new Fiber project at {}", dir.display());
+fn init_command(dir: PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+    fs::create_dir_all(&dir)?;
+    fs::create_dir_all(dir.join("src"))?;
+
+    // get the directory name, if dir is '.' get the current directory name
+    let abs_path = dir.canonicalize()?;
+    let package_name = abs_path.to_str().unwrap().split('/').last().unwrap();
+
+    let config_template = CONFIG_TEMPLATE_STR;
+    let mut tera = Tera::default();
+    let mut context = Context::new();
+    tera.add_raw_template("default_config", config_template)?;
+    context.insert("package_name", &package_name);
+    context.insert("language_version", VERSION);
+    let config = tera.render("default_config", &context)?;
+
+    fs::write(dir.join(CONFIG_PATH), config)?;
+
+    let source_template = SOURCE_TEMPLATE_STR;
+
+    fs::write(dir.join(SRC_ROOT).join(MAIN_ENTRY), source_template)?;
+
+    Ok(())
 }
 
-fn deps_command(urls: Vec<String>, dest: Option<PathBuf>) {
-	let dest = dest.unwrap_or_else(|| PathBuf::from("deps"));
-	fs::create_dir_all(&dest).unwrap_or_else(|e| {
-		eprintln!("Failed to create deps dir: {}", e);
-		process::exit(1);
-	});
+fn deps_command(
+    urls: Vec<String>,
+    dest: Option<PathBuf>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let dest = dest.unwrap_or_else(|| PathBuf::from("deps"));
+    fs::create_dir_all(&dest).unwrap_or_else(|e| {
+        eprintln!("Failed to create deps dir: {}", e);
+        process::exit(1);
+    });
 
-	for url in urls {
-		match ureq::get(&url).call() {
-			Ok(response) => {
-				let fname = url.split('/').last().unwrap_or("file.bin");
-				let path = dest.join(fname);
-				let mut out = fs::File::create(&path).unwrap_or_else(|e| {
-					eprintln!("Failed to create {}: {}", path.display(), e);
-					process::exit(1);
-				});
-				let mut reader = response.into_reader();
-				if let Err(e) = std::io::copy(&mut reader, &mut out) {
-					eprintln!("Failed to write {}: {}", path.display(), e);
-					process::exit(1);
-				}
-				println!("Downloaded {} -> {}", url, path.display());
-			}
-			Err(e) => {
-				eprintln!("Failed to fetch {}: {}", url, e);
-				process::exit(1);
-			}
-		}
-	}
+    for url in urls {
+        match ureq::get(&url).call() {
+            Ok(response) => {
+                let fname = url.split('/').last().unwrap_or("file.bin");
+                let path = dest.join(fname);
+                let mut out = fs::File::create(&path).unwrap_or_else(|e| {
+                    eprintln!("Failed to create {}: {}", path.display(), e);
+                    process::exit(1);
+                });
+                let mut reader = response.into_reader();
+                if let Err(e) = std::io::copy(&mut reader, &mut out) {
+                    eprintln!("Failed to write {}: {}", path.display(), e);
+                    process::exit(1);
+                }
+                println!("Downloaded {} -> {}", url, path.display());
+            }
+            Err(e) => {
+                eprintln!("Failed to fetch {}: {}", url, e);
+                process::exit(1);
+            }
+        }
+    }
+    Ok(())
 }
