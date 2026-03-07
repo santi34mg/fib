737622cf6eeb19ad2ecfd479ec17cad682cbeaef
santi34mg <ssilvera@correo.um.edu.uy>
2026-03-07 16:09:26 -0300

    updated parser

--- fiberc/src/parser/test.rs
+++ fiberc/src/parser/test.rs
@@ -1,8 +1,8 @@
 #[cfg(test)]
 mod tests {
-    use crate::ast::ast::{
-        DeclarationNode, Expression, Field, PointerVariant, TypeIdentifier, VariableDeclaration,
-    };
+    use std::path::Path;
+
+    use crate::ast::ast::{DeclarationNode, Expression, TypeExpression, VariableDeclaration};
     use crate::ast::{Ast, StatementNode};
     use crate::lexer::Lexer;
     use crate::parser::Parser;
@@ -17,8 +17,8 @@ mod tests {
         };
         let lexer = Lexer::new(&src);
         let tokens: Vec<Token> = lexer.collect();
-        let mut parser = Parser::new(tokens.into_iter(), "test_instance", src.clone());
-        match parser.parse_module() {
+        let mut parser = Parser::new(tokens.into_iter(), Path::new("test_instance"), src.clone());
+        match parser.parse() {
             Ok(ast) => ast,
             Err(e) => panic!("Could not parse {}, error:\n{}", src, e),
         }
@@ -26,12 +26,10 @@ mod tests {
 
     fn module_statements(ast: &Ast) -> Vec<&StatementNode> {
         let mut stmts = Vec::new();
-        if let Some(module) = ast.program.modules.get(0) {
-            for decl in &module.declarations {
+            for decl in &ast.declarations {
                 if let DeclarationNode::Statement(s) = decl {
                     stmts.push(s);
                 }
-            }
         }
         stmts
     }
@@ -40,7 +38,6 @@ mod tests {
     fn test_expression_literal() {
         let test_string = "1";
         let ast = get_ast(test_string);
-        assert_eq!(ast.program.modules.len(), 1);
         let stmts = module_statements(&ast);
         assert_eq!(stmts.len(), 1);
         assert!(matches!(
@@ -173,11 +170,11 @@ mod tests {
         assert_eq!(stmts.len(), 1);
         if let StatementNode::VariableDeclaration(VariableDeclaration {
             identifier,
-            variable_type: Some(TypeIdentifier::Integer),
+            variable_type: Some(TypeExpression::Identifier(_)),
             expression: Some(Expression::Literal(Literal::Integer(5))),
         }) = stmts[0]
         {
-            assert_eq!(identifier, "x");
+            assert_eq!(identifier.identifier, "x");
         } else {
             panic!("AST statement did not match expected VariableDeclaration");
         }
@@ -195,7 +192,7 @@ mod tests {
             expression: Some(Expression::Literal(Literal::Integer(5))),
         }) = stmts[0]
         {
-            assert_eq!(identifier, "x");
+            assert_eq!(identifier.identifier, "x");
         } else {
             panic!("AST statement did not match expected VariableDeclaration");
         }
@@ -209,11 +206,11 @@ mod tests {
         assert_eq!(stmts.len(), 1);
         if let StatementNode::VariableDeclaration(VariableDeclaration {
             identifier,
-            variable_type: Some(TypeIdentifier::Integer),
+            variable_type: Some(TypeExpression::Identifier(_)),
             expression: None,
         }) = stmts[0]
         {
-            assert_eq!(identifier, "x");
+            assert_eq!(identifier.identifier, "x");
         } else {
             panic!("AST statement did not match expected VariableDeclaration");
         }
@@ -227,11 +224,11 @@ mod tests {
         assert_eq!(stmts.len(), 1);
         if let StatementNode::VariableDeclaration(VariableDeclaration {
             identifier,
-            variable_type: Some(TypeIdentifier::Integer),
+            variable_type: Some(TypeExpression::Identifier(_)),
             expression: Some(Expression::Literal(Literal::Integer(5))),
         }) = stmts[0]
         {
-            assert_eq!(identifier, "x");
+            assert_eq!(identifier.identifier, "x");
         } else {
             panic!("AST statement did not match expected VariableDeclaration");
         }
@@ -249,165 +246,9 @@ mod tests {
             expression: None,
         }) = stmts[0]
         {
-            assert_eq!(identifier, "x");
+            assert_eq!(identifier.identifier, "x");
         } else {
             panic!("AST statement did not match expected VariableDeclaration");
         }
-    }
-
-    fn test_type(type_string: String, expected_type: TypeIdentifier) {
-        let test_string = format!("let x {};", type_string);
-        let ast = get_ast(&test_string);
-        let stmts = module_statements(&ast);
-        assert_eq!(stmts.len(), 1);
-        if let StatementNode::VariableDeclaration(VariableDeclaration {
-            identifier,
-            variable_type: Some(var_type),
-            expression: None,
-        }) = stmts[0]
-        {
-            assert_eq!(identifier, "x");
-            assert_eq!(*var_type, expected_type);
-        } else {
-            panic!("AST statement did not match expected VariableDeclaration");
-        }
-    }
-
-    #[test]
-    fn test_type_integer() {
-        test_type("int".to_string(), TypeIdentifier::Integer);
-    }
-
-    #[test]
-    fn test_type_float() {
-        test_type("float".to_string(), TypeIdentifier::Float);
-    }
-
-    #[test]
-    fn test_type_bool() {
-        test_type("bool".to_string(), TypeIdentifier::Boolean);
-    }
-
-    #[test]
-    fn test_type_char() {
-        test_type("char".to_string(), TypeIdentifier::Character);
-    }
-
-    #[test]
-    fn test_type_unit() {
-        test_type("unit".to_string(), TypeIdentifier::Unit);
-    }
-
-    #[test]
-    fn test_type_string() {
-        test_type("string".to_string(), TypeIdentifier::String);
-    }
-
-    #[test]
-    fn test_type_dynamic() {
-        test_type("dynamic".to_string(), TypeIdentifier::Dynamic);
-    }
-
-    #[test]
-    fn test_type_blob() {
-        test_type("blob".to_string(), TypeIdentifier::Blob);
-    }
-
-    #[test]
-    fn test_type_never() {
-        test_type("never".to_string(), TypeIdentifier::Never);
-    }
-
-    #[test]
-    fn test_type_raw_pointer() {
-        test_type(
-            "&int".to_string(),
-            TypeIdentifier::Pointer {
-                pointer_variant: PointerVariant::Raw,
-                pointed_type: Box::new(TypeIdentifier::Integer),
-            },
-        );
-    }
-
-    #[test]
-    fn test_type_unique_pointer() {
-        test_type(
-            "unique &int".to_string(),
-            TypeIdentifier::Pointer {
-                pointer_variant: PointerVariant::Unique,
-                pointed_type: Box::new(TypeIdentifier::Integer),
-            },
-        );
-    }
-
-    #[test]
-    fn test_type_shared_pointer() {
-        test_type(
-            "shared &int".to_string(),
-            TypeIdentifier::Pointer {
-                pointer_variant: PointerVariant::Shared,
-                pointed_type: Box::new(TypeIdentifier::Integer),
-            },
-        );
-    }
-
-    #[test]
-    fn test_type_weak_pointer() {
-        test_type(
-            "weak &int".to_string(),
-            TypeIdentifier::Pointer {
-                pointer_variant: PointerVariant::Weak,
-                pointed_type: Box::new(TypeIdentifier::Integer),
-            },
-        );
-    }
-
-    #[test]
-    fn test_type_struct() {
-        test_type(
-            "struct { x int, y float }".to_string(),
-            TypeIdentifier::Struct {
-                fields: vec![
-                    Field {
-                        label: "x".to_string(),
-                        type_id: TypeIdentifier::Integer,
-                    },
-                    Field {
-                        label: "y".to_string(),
-                        type_id: TypeIdentifier::Float,
-                    },
-                ],
-            },
-        );
-    }
-
-    #[test]
-    fn test_type_variant() {
-        test_type(
-            "variant { x int, y float }".to_string(),
-            TypeIdentifier::Variant {
-                fields: vec![
-                    Field {
-                        label: "x".to_string(),
-                        type_id: TypeIdentifier::Integer,
-                    },
-                    Field {
-                        label: "y".to_string(),
-                        type_id: TypeIdentifier::Float,
-                    },
-                ],
-            },
-        );
-    }
-
-    #[test]
-    fn test_type_function_simple() {
-        test_type(
-            "function(int) -> bool".to_string(),
-            TypeIdentifier::Function {
-                argument_types: vec![TypeIdentifier::Integer],
-                return_type: Box::new(TypeIdentifier::Boolean),
-            },
-        );
     }
 }
