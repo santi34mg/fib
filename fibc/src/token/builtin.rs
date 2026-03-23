2dd20d7e33f23e9729d7a39a545800aa7eb52824
santi34mg <ssilvera@correo.um.edu.uy>
2026-03-23 18:57:49 -0300

    feat: rename sint types to int and add never builtin type
    Sint8/16/32/64 renamed to Int8/16/32/64 for simpler naming convention.
    Added Never type (bottom type for non-returning functions).

--- fibc/src/token/builtin.rs
+++ fibc/src/token/builtin.rs
@@ -13,10 +13,10 @@ pub enum BuiltinType {
     UInt16  = 2,
     UInt32  = 3,
     UInt64  = 4,
-    SInt8   = 5,
-    SInt16  = 6,
-    SInt32  = 7,
-    SInt64  = 8,
+    Int8    = 5,
+    Int16   = 6,
+    Int32   = 7,
+    Int64   = 8,
     Float16 = 9,
     Float32 = 10,
     Float64 = 11,
@@ -24,6 +24,7 @@ pub enum BuiltinType {
     Char    = 13,
     Boolean = 14,
     String  = 15,
+    Never   = 16,
 }
 
 impl fmt::Display for BuiltinType {
@@ -34,10 +35,10 @@ impl fmt::Display for BuiltinType {
             Self::UInt16 => write!(f, "uint16"),
             Self::UInt32 => write!(f, "uint32"),
             Self::UInt64 => write!(f, "uint64"),
-            Self::SInt8 => write!(f, "sint8"),
-            Self::SInt16 => write!(f, "sint16"),
-            Self::SInt32 => write!(f, "sint32"),
-            Self::SInt64 => write!(f, "sint64"),
+            Self::Int8 => write!(f, "int8"),
+            Self::Int16 => write!(f, "int16"),
+            Self::Int32 => write!(f, "int32"),
+            Self::Int64 => write!(f, "int64"),
             Self::Float16 => write!(f, "float16"),
             Self::Float32 => write!(f, "float32"),
             Self::Float64 => write!(f, "float64"),
@@ -45,6 +46,7 @@ impl fmt::Display for BuiltinType {
             Self::Char => write!(f, "char"),
             Self::Boolean => write!(f, "bool"),
             Self::String => write!(f, "string"),
+            Self::Never => write!(f, "never"),
         }?;
         Ok(())
     }
